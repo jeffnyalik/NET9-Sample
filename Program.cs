9f50a7fd028a8c97b8403274866408064ede8469
Jeff Nyalik <jeffnyak@gmail.com>
2024-12-16 21:19:15 +0300

    NET NLogger & Writing to folder

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.configureCors();
 builder.Services.configureIIsConfiguration();
+builder.Services.configureLoggerServices();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
